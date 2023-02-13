b9974d896c91737b24ebe63154995a1ddd3ec08e
rovxxmjxx <miju3545@gmail.com>
2023-02-13 15:33:54 +0900

    feat: setting

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+                                                                                return (
+                                                                                                                                                                <div>
+                                                                                                                                                                                                                                                초기설정
+                                                                                                                                                                </div>
+                                                                                );
+}
+
+export default App;
