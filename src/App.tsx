5c4a68fe5eedc7fc34bab4acf6187cdc8eced66b
rovxxmjxx <miju3545@gmail.com>
2023-02-14 16:39:58 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Router from './router';
+import Layout from './components/Layout';
 
 function App() {
-                                                                                return (
-                                                                                                                                                                <div>
-                                                                                                                                                                                                                                                초기설정
-                                                                                                                                                                </div>
-                                                                                );
+  return (
+    <Layout>
+      <Router />
+    </Layout>
+  );
 }
 
 export default App;
