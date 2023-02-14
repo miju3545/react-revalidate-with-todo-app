438be3f2f82f1ad58c79c141845363576f7a8947
rovxxmjxx <miju3545@gmail.com>
2023-02-14 16:57:31 +0900

    refactor: reset method 추가

--- src/hooks/useInputAsync.ts
+++ src/hooks/useInputAsync.ts
@@ -1,6 +1,10 @@
 import { ChangeEvent, useState, useEffect } from 'react';
 
-type ReturnType<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void];
+type ReturnType<T> = [
+  T,
+  (e: ChangeEvent<HTMLInputElement>) => void,
+  () => void
+];
 
 const useInputAysnc = <T>(initialValue: T): ReturnType<T> => {
   const [value, setValue] = useState(initialValue);
@@ -12,11 +16,13 @@ const useInputAysnc = <T>(initialValue: T): ReturnType<T> => {
     }
   };
 
+  const reset = () => setValue(initialValue);
+
   useEffect(() => {
     setValue(initialValue);
   }, [initialValue]);
 
-  return [value, handler];
+  return [value, handler, reset];
 };
 
 export default useInputAysnc;
