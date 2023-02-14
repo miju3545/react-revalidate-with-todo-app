e1876a8e455e753c9a9ad240e50873438780282f
rovxxmjxx <miju3545@gmail.com>
2023-02-14 16:39:25 +0900

    feat: todoitem component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,124 @@
+import React, { useReducer, memo, KeyboardEvent, ChangeEvent } from 'react';
+import { Todo } from '../utils/types';
+import Button from './Button';
+import useInput from '../hooks/useInput';
+import styled from '@emotion/styled';
+import useInputAysnc from '../hooks/useInputAsync';
+
+type TodoProps = Todo & {
+  onUpdate: (id: number, body: { isCompleted: boolean; todo: string }) => void;
+  onDelete: (id: number) => void;
+};
+
+function TodoItem({ id, todo, isCompleted, onUpdate, onDelete }: TodoProps) {
+  const [completed, toggleCompleted] = useInputAysnc(isCompleted);
+  const [editedTodo, editing] = useInputAysnc(todo);
+  const [showEditForm, toggleShowEditForm] = useReducer((p) => !p, false);
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleUpdate();
+      toggleShowEditForm();
+    }
+  };
+
+  const handleUpdate = () => {
+    onUpdate(id, {
+      isCompleted: completed,
+      todo: editedTodo,
+    });
+  };
+
+  const handleUpdateCompleted = (e: ChangeEvent<HTMLInputElement>) => {
+    toggleCompleted(e);
+    onUpdate(id, { isCompleted: e.target.checked, todo: editedTodo });
+  };
+
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <ItemWrapper>
+      <Form>
+        <Label>
+          <Checkbox
+            type="checkbox"
+            checked={completed}
+            onChange={handleUpdateCompleted}
+          />
+          <Text className="textbox">
+            {showEditForm ? (
+              <input
+                value={editedTodo}
+                onChange={editing}
+                onKeyDown={handleKeyDown}
+              />
+            ) : (
+              <span>{todo}</span>
+            )}
+          </Text>
+        </Label>
+        {showEditForm ? (
+          <>
+            <Button
+              id="submit-button"
+              title="제출"
+              type="button"
+              onClick={handleUpdate}
+            />
+            <Button
+              id="cancel-button"
+              title="취소"
+              type="button"
+              onClick={toggleShowEditForm}
+            />
+          </>
+        ) : (
+          <>
+            <Button
+              id="modify-button"
+              title="수정"
+              type="button"
+              onClick={toggleShowEditForm}
+            />
+            <Button
+              id="delete-button"
+              title="삭제"
+              type="button"
+              onClick={handleDelete}
+            />
+          </>
+        )}
+      </Form>
+    </ItemWrapper>
+  );
+}
+
+export default memo(TodoItem);
+
+const ItemWrapper = styled.li`
+  list-style: none;
+  padding: 10px 0;
+  border-bottom: 1px solid #dfdfdf;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+`;
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+`;
+const Checkbox = styled.input`
+  font-size: 14px;
+  margin-right: 5px;
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+  margin-right: 10px;
+`;
